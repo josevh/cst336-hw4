7fa410a16c61b5954d52e9d60d024e600931b42a
Jose Herrera <josevictorherrera@gmail.com>
2020-11-19 20:15:14 -0800

    Use faker fake data

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
 const app = express()
+const faker = require('faker')
 
 // express setup
 app.set('view engine', 'ejs');
@@ -9,13 +10,21 @@ app.use(expressLayouts)
 
 // routes
 app.get("/", function (req, res) {
-    res.render("index")
+    res.render("index", {fakeContent: faker.lorem.paragraph()})
 })
 app.get("/application", function (req, res) {
-    res.render("application")
+    res.render("application", {
+        host: faker.internet.domainName(),
+        userAgent: faker.internet.userAgent(),
+    })
 })
 app.get("/network", function (req, res) {
-    res.render("network")
+    res.render("network", {
+        fakeIP1: faker.internet.ip(),
+        fakeIP2: faker.internet.ip(),
+        fakeIP3: faker.internet.ip(),
+        fakeIPv6: faker.internet.ipv6()
+    })
 })
 app.get("/transport", function (req, res) {
     res.render("transport")
