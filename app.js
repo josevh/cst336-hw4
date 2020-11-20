d91905d58c6359fdd244c2d28f01ccdc2e748bef
Jose Herrera <josevictorherrera@gmail.com>
2020-11-19 20:30:01 -0800

    Don't specify IP

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const expressLayouts = require('express-ejs-layouts')
 const app = express()
 const faker = require('faker')
 
-const IP = process.env.IP || '0.0.0.0'
 const PORT = process.env.PORT || 5000
 
 // express setup
@@ -35,6 +34,6 @@ app.get("/transport", function (req, res) {
 
 
 // starting server
-app.listen(PORT, IP, function () {
-    console.log("Express server is running at " + process.env.IP + ":" + process.env.PORT)
+app.listen(PORT, function () {
+    console.log("Express server is running...")
 })
