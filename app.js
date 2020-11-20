f6634513c26c6a270641ee084573aa7508899133
Jose Herrera <josevictorherrera@gmail.com>
2020-11-19 20:22:55 -0800

    Refactor default IP and port

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const expressLayouts = require('express-ejs-layouts')
 const app = express()
 const faker = require('faker')
 
+const IP = process.env.IP || '0.0.0.0'
+const PORT = process.env.PORT || 5000
+
 // express setup
 app.set('view engine', 'ejs');
 app.use(express.static("public"))
@@ -32,6 +35,6 @@ app.get("/transport", function (req, res) {
 
 
 // starting server
-app.listen(process.env.PORT, process.env.IP, function () {
+app.listen(PORT, IP, function () {
     console.log("Express server is running at " + process.env.IP + ":" + process.env.PORT)
 })
