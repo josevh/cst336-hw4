710cfbda2390f8fa10c49f189e182fd9439dd4c7
Jose Herrera <josevictorherrera@gmail.com>
2020-11-19 20:32:38 -0800

    Add start script

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const expressLayouts = require('express-ejs-layouts')
 const app = express()
 const faker = require('faker')
 
+const IP = process.env.IP || '0.0.0.0'
 const PORT = process.env.PORT || 5000
 
 // express setup
@@ -34,6 +35,6 @@ app.get("/transport", function (req, res) {
 
 
 // starting server
-app.listen(PORT, function () {
-    console.log("Express server is running...")
+app.listen(PORT, IP, function () {
+    console.log("Express server is running at " + IP + ":" + PORT)
 })
